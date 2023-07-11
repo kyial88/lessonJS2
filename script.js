9b174f159caf0b54266463fa6dcc50dd5e5e4e3f
kyial88 <klibraeva@gmail.com>
2023-07-11 17:20:43 +0600

    hw2

---
+++ script.js
@@ -0,0 +1,77 @@
+//  let a = 'hello'
+// let b = 'Ivan'
+// let c = a.slice(1)
+//  let d = a[0]
+//  let f =b.slice(1)
+// let e = b[0]
+//  console.log(d.toUpperCase()+ c+' '+b)
+ // Hello Ivan
+
+
+
+ // let a = 'lorem ipsum dolor sit amet'
+// let b = a.length
+// console.log(b)
+
+// let a = 'lorem ipsum dolor sit amet'
+//  sit lorem ipsum dolor amet
+// let b = a.slice(a.lastIndexOf('s'),a.lastIndexOf('s')+3)
+// let c = a.slice(0,a.lastIndexOf('r')+1)
+// console.log(b+''+c+''+ a.slice(a.lastIndexOf('a')))
+
+ // let a = ' children'
+//  CHILDREn
+// let b = a.slice(0,8).toUpperCase()
+// let c = a.slice(-1).toLowerCase()
+// console.log(b+c)
+
+// let a = 'boss'
+// bsso
+// console.log(a[0] + a.slice(a.indexOf('s'), a.indexOf('s') + 2) + a[1])
+
+// let a = 'sunday'
+// console.log((a[0,3]))
+
+// let a = 'hello'
+// console.log(a.replace('ll','ks'))
+
+
+// let a = 'user'
+// let b = 'new'
+// let c = a.slice(0,2)
+// console.log(b+c)
+
+//  let a = 'monday'
+// let b='day'
+// let c = a.slice(0,3)
+// // // вернуть daydaymon)
+// console.log(b+b+c)
+
+// let a = 'HELLO'
+// // hElLo
+// let b = a.slice(0)
+// let c =a.slice(-1)
+//
+// //  console.log(a+b+c)
+// let a = '5'
+// let b = '400'
+// console.log(a+b)
+
+// let a = 'monkey'
+// let b = 'banana'
+// // вернуть manak
+// console.log(a.replace(a+b)[2]+a.replace(a+b)[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
